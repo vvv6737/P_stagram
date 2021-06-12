fd4a32806e67da84c02b449d769ad4a7e461e563
jeonghoon <vvv6737@naver.com>
2021-06-13 03:10:19 +0900

    update

---
+++ P-stagram/app/src/main/java/com/SNS/p_stagram/navigation/AddPhotoActivity.kt
@@ -0,0 +1,15 @@
+package com.SNS.p_stagram.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.SNS.p_stagram.navigation.AlarmFragment
+import com.SNS.p_stagram.navigation.DetailViewFragment
+import com.SNS.p_stagram.navigation.GridFragment
+import com.SNS.p_stagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class AddPhotoActivity : AppCompatActivity(){
+    var
+}
