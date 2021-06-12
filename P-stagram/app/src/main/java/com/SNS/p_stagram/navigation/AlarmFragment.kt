581fe60cc3554d7304c014d3b3c77daf54af7d30
jeonghoon <vvv6737@naver.com>
2021-06-12 16:54:46 +0900

    네비게이션바, 메뉴 추가

---
+++ P-stagram/app/src/main/java/com/SNS/p_stagram/navigation/AlarmFragment.kt
@@ -0,0 +1,19 @@
+package com.SNS.p_stagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.SNS.p_stagram.R
+
+class AlarmFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
+        return view
+    }
+}
